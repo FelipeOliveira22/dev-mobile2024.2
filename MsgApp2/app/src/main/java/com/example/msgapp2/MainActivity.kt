5c75f749d3d5bc09ade65b7bd727464ff9e18418
FelipeOliveira22 <felipe.olv@alu.ufc.br>
2025-01-24 20:14:21 -0300

    Creating MsgApp2

---
+++ MsgApp2/app/src/main/java/com/example/msgapp2/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.msgapp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
+import com.example.msgapp2.data.local.database.AppDataBase
+import com.example.msgapp2.repository.MessageRepository
+import com.example.msgapp2.ui.theme.MsgApp2Theme
+import com.example.msgapp2.ui.view.MessageApp
+import com.example.msgapp2.viewModel.MessageViewModel
+import com.example.msgapp2.viewModel.MessageViewModelFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDataBase::class.java, "messages-db"
+        ).fallbackToDestructiveMigration().build()
+
+        val repository = MessageRepository(db.messageDao())
+        enableEdgeToEdge()
+        setContent {
+            MsgApp2Theme {
+                val viewModel: MessageViewModel = viewModel(factory = MessageViewModelFactory(repository))
+                MessageApp(viewModel)
+            }
+        }
+    }
+}
---
+++ MsgApp2/app/src/main/java/com/example/msgapp2/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package com.example.msgapp2.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    onPrimary = Color.White,
+    secondary = Color(0xFF03DAC5),
+    onSecondary = Color.Black,
+    surface = Color(0xFFF5F5F5),
+    onSurface = Color.Black,
+    background = Color(0xFFF5F5F5),
+    onBackground = Color.Black
+)
+
+@Composable
+fun MsgApp2Theme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = Typography,
+        content = content
+    )
+}
